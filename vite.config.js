61b885205b4d840bd379b8db5b6ddf5609affb71
Hamza Oubaha <mahzaboha@gmail.com>
2026-05-17 13:59:53 +0100

    Make vite base path conditional for Vercel support

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/takhfidat_lamkanssa/'
+  base: process.env.VERCEL ? '/' : '/takhfidat_lamkanssa/'
 })
